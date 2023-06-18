2430ba1ad43e886190a4e09c9e30af905b0266fb
quavodanceq <kuat.oralbekov1011@mail.ru>
2023-06-18 13:02:27 +0600

    Initial Commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Куат Оралбеков on 18.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
